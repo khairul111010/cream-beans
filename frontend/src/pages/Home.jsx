2bfcacca92ed572058ce9494a7fcdf28918d93a1
XWSHAMIM <xwshamim1@gmail.com>
2023-12-14 14:25:39 +0600

    Added Carousel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,10 @@
-const Home = () => {
+import CarouselDefault from "../components/Carosel";
+function Home() {
   return (
     <>
-      {/* <video className=" w-full rounded-lg max-h-90" controls autoPlay>
-        <p>we will rock you</p>
-        <source
-          src="https://docs.material-tailwind.com/demo.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video> */}
+      <CarouselDefault />
     </>
   );
-};
+}
 
 export default Home;
