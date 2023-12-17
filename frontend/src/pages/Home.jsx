959fd2190397e7ab3c1a086d3bc119e3c5bb85ee
khairul111010 <khairul18103199@gmail.com>
2023-12-17 16:41:31 +0600

    service --working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import gsap from "gsap";
 import { useLayoutEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import Clutch from "../assets/clutch.svg";
+import Fiverr from "../assets/fiverr.svg";
 import Hero from "../assets/hero.svg";
 import Upwork from "../assets/upwork.svg";
-import Fiverr from "../assets/fiverr.svg";
-import Clutch from "../assets/clutch.svg";
 
 const Home = () => {
   const heroRef = useRef(null);
@@ -27,24 +27,28 @@ const Home = () => {
           xPercent: -100,
           ease: "power4.out",
           delay: 0.3,
-        }).from([sublineRef.current,buttonRef.current],{
-          opacity: 0,
-          ease: "power4.out",
-          delay: 0.1,
-          stagger: 0.6,
-          onComplete: function () {
-            sessionStorage.setItem(
-              import.meta.env.VITE_HASPLAYEDHEADLINE,
-              true
-            );
-          },
-        });
+        })
+          .to(headlineRef.current, {
+            zIndex: -50,
+          })
+          .from([sublineRef.current, buttonRef.current], {
+            opacity: 0,
+            ease: "power4.out",
+            delay: 0.1,
+            stagger: 0.6,
+            onComplete: function () {
+              sessionStorage.setItem(
+                import.meta.env.VITE_HASPLAYEDHEADLINE,
+                true
+              );
+            },
+          });
 
-        t2.from(heroBannerRef.current,{
+        t2.from(heroBannerRef.current, {
           opacity: 0,
           ease: "power4.out",
           delay: 0.4,
-        })
+        });
       }
     }, [heroRef]);
     return () => ctx.revert();
@@ -57,7 +61,10 @@ const Home = () => {
           ref={heroRef}
           className="flex flex-col justify-center gap-6 flex-grow"
         >
-          <h1 ref={headlineRef} className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          <h1
+            ref={headlineRef}
+            className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl"
+          >
             We{" "}
             <span className="font-bold text-primary">
               develop, fix <span className="font-normal text-black">&</span>{" "}
@@ -89,7 +96,10 @@ const Home = () => {
         </div>
       </div>
       <div className="md:max-w-[80%] max-w-[90%] mx-auto border py-3 md:py-10 rounded-lg bg-brown-200/20 mb-6">
-        <div className="text-center md:text-xl text-sm">We are also <span className="text-primary font-bold">available</span> on</div>
+        <div className="text-center md:text-xl text-sm">
+          We are also <span className="text-primary font-bold">available</span>{" "}
+          on
+        </div>
         <div className="flex items-center justify-center gap-16 rounded-lg md:py-10 md:px-3 py-3 px-7">
           <div>
             <img src={Upwork} alt="" className="h-11 w-auto" />
@@ -101,6 +111,13 @@ const Home = () => {
             <img src={Fiverr} alt="" className="h-11 w-auto" />
           </div>
         </div>
+      </div>
+
+      <div className="md:max-w-[80%] max-w-[90%] mx-auto">
+        <div className="md:text-4xl md:font-bold text-secondary text-center">
+          What do we do?
+        </div>
+        <div className="mt-7"></div>
       </div>
     </>
   );
