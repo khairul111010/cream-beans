e1a8b80b13598df443d895045d9cbbae05b52d15
khairul111010 <khairul18103199@gmail.com>
2023-12-21 23:18:08 +0600

    service changes

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -16,7 +16,7 @@ import ui from "../assets/ui.svg";
 
 const Services = () => {
   return (
-    <div className="grid md:grid-cols-3 grid-cols-1 mt-4 gap-8 md:max-w-[80%] max-w-[90%] mx-auto overflow-hidden">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 mt-4 gap-8 md:max-w-[80%] max-w-[90%] mx-auto overflow-hidden">
       <div className="bg-white rounded-lg px-10 pb-10 pt-14 shadow transition-all duration-100 ease-in-out flex flex-col items-center gap-4">
         <div>
           <img src={web} alt="" srcset="" className="h-14 w-auto" />
